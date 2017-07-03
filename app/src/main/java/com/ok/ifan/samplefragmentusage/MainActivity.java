90b52c8397865b783d6eebd0c242884ca530462d
Ifan OK <ifannuddin@gmail.com>
2017-07-03 19:29:35 +0700

    Initial commit

---
+++ app/src/main/java/com/ok/ifan/samplefragmentusage/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ok.ifan.samplefragmentusage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ok.ifan.samplefragmentusage.fragment.FirstFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.frame_container,
+                            new FirstFragment(),
+                            FirstFragment.class.getSimpleName()).commit();
+        }
+    }
+}
